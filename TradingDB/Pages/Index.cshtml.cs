d0fa62e6ef6c81848a413000f47e316f02544e70
Neeraj <62223906+MrAvatin@users.noreply.github.com>
2021-12-16 21:47:13 -0700

    initial

---
+++ TradingDB/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TradingDB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPostAdminLogin() {
+            return RedirectToPage("AdminPage");
+        }
+
+        public IActionResult OnPostClientLogin()
+        {
+            return RedirectToPage("ClientPage");
+        }
+    }
+}
