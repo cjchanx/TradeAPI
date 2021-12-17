40e142eb3a8761619535e3f40f44ba806ddeca48
Chris <78698227+cjchanx@users.noreply.github.com>
2021-12-17 13:05:12 -0700

    Implementation of basic API endpoints for Account.

---
+++ Webservice/Controllers/AccountsController.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Webservice.Configuration;
+using Webservice.ContextHelpers;
+using Webservice.ControllerHelpers;
+using TradingLibrary.Models;
+using DatabaseLibrary.Helpers;
+using System.Net;
+
+namespace Webservice.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        #region Intialization
+        /// <summary>
+        /// Reference to hosting environment instance.
+        /// </summary>
+        private readonly IWebHostEnvironment HostingEnvironment;
+
+        /// <summary>
+        /// Reference to app settings helper.
+        /// </summary>
+        private readonly AppSettingsHelper AppSettings;
+
+        /// <summary>
+        /// Reference to the DBContextHelper instance.
+        /// </summary>
+        private readonly DatabaseContextHelper Database;
+
+        #endregion
+
+        // API
+
+        [HttpGet]
+        [Route("GetAccounts")]
+        public ResponseMessage GetAccounts()
+        {
+            var response = AccountHelper.GetCollection(Database.DBContext, out HttpStatusCode stat, HostingEnvironment.IsDevelopment());
+            HttpContext.Response.StatusCode = (int)stat;
+            return response;
+        }
+
+        [HttpPost]
+        [Route("AddAccount")]
+        public ResponseMessage AddAccount([FromBody] JObject data)
+        {
+            var resp = AccountHelper.Add(data, Database.DBContext, out HttpStatusCode stat, HostingEnvironment.IsDevelopment());
+            HttpContext.Response.StatusCode = (int)stat;
+            return resp;
+        }
+    }
+}
