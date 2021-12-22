65d83fdc44613309e68dcfc5fb6db93b118dab14
Neeraj <62223906+MrAvatin@users.noreply.github.com>
2021-12-22 03:39:47 -0700

    dropdonw for action

--- TradingLibrary/Enums/OrderAction.cs
+++ TradingLibrary/Enums/OrderAction.cs
@@ -8,9 +8,9 @@ namespace TradingLibrary.Enums
 {
     public enum OrderAction
     {
-        Upper_limit_sell, // For sell orders where the limit is higher than the current
-        Lower_limit_sell, // For sell orders where the limit is lower than the current
-        Upper_limit_buy, // For buy orders where the limit is higher than the current
-        Lower_limit_buy // For buy orders where the limit is lower than the current
+        Upper_limit_sell = 0, // For sell orders where the limit is higher than the current
+        Lower_limit_sell = 1, // For sell orders where the limit is lower than the current
+        Upper_limit_buy = 2, // For buy orders where the limit is higher than the current
+        Lower_limit_buy = 3 // For buy orders where the limit is lower than the current
     }
 }
--- Webservice/Pages/AdminSecurityPage.cshtml.cs
+++ Webservice/Pages/AdminSecurityPage.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace Webservice.Pages
+using Webservice.ContextHelpers;
+using DatabaseLibrary.Helpers;
+using DatabaseLibrary.Core;
+using TradingLibrary.Models;
+namespace TradingDB.Pages
 {
-    public class AdminSecurityPageModel : PageModel
+    public class AdminSecurityPageModel : Controller
     {
+
+        private readonly DatabaseContextHelper _context;
+
+        public AdminSecurityPageModel(DatabaseContextHelper context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult Delete(string symbol)
+        {
+            Console.WriteLine("Remove " + symbol);
+            return RedirectToPage("/AdminSecurityPage");
         }
     }
 }
