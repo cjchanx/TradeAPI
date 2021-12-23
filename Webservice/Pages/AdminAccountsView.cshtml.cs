e1d42b701acc0b76bad612ca8a2778f692cb2fd4
Neeraj <62223906+MrAvatin@users.noreply.github.com>
2021-12-22 18:35:17 -0700

    everything work

--- Webservice/Pages/AdminAccountsView.cshtml.cs
+++ Webservice/Pages/AdminAccountsView.cshtml.cs
@@ -1,15 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Webservice.ContextHelpers;
 using DatabaseLibrary.Helpers;
 using DatabaseLibrary.Core;
-using Webservice.Controllers;
 using TradingLibrary.Models;
-namespace Webservice.Pages
+namespace TradingDB.Pages
 {
-    public class AdminAccountsViewModel : PageModel
+    public class AdminAccountsViewModel : Controller
     {
+
+        private readonly DatabaseContextHelper _context;
+
+        public AdminAccountsViewModel(DatabaseContextHelper context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult Delete(int id)
+        {
+            AccountsHelper_db.ForceDelete(_context.DBContext, out StatusResponse response, id);
+            return RedirectToPage("/AdminAccountsView");
         }
     }
 }
